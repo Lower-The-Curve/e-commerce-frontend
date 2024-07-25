dbe1aaf996c928b7af3590bb2e4e588ca1387ce6
Albin Izmaku <albinizmaku2@gmail.com>
2024-07-25 19:51:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppleIcon from "./svg/apple";
+import FacebookIcon from "./svg/facebook";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="card">
+        <div>
+          <a href="https://www.shopify.com/" target="_blank" rel="noreferrer">
+            <img src="./logo.svg" alt="Logo" width={92} height={27} />
+          </a>
+        </div>
+        <div>
+          <div>
+            <h1>Log in</h1>
+            <h3>Continue to Shopify account</h3>
+          </div>
+          <form>
+            <label htmlFor="email">Email</label>
+            <input type="email" />
+            <button type="submit">Continue with email</button>
+            <button type="submit">Sign in with passkey</button>
+          </form>
+          <p className="sideways">
+            <span>or</span>
+          </p>
+        </div>
+        <div>
+          <AppleIcon />
+        </div>
+      </div>
     </div>
   );
 }
