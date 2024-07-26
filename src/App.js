d581112564d7cb47efde5739e7155d33d1a0b8d5
Albin Izmaku <albinizmaku2@gmail.com>
2024-07-26 13:52:28 +0200

    Finshed structure of Login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import AppleIcon from "./svg/apple";
+import RightArrow from "./svg/arrow";
 import FacebookIcon from "./svg/facebook";
+import GoogleIcon from "./svg/google";
+import User from "./svg/user";
 
 function App() {
   return (
@@ -11,23 +14,47 @@ function App() {
             <img src="./logo.svg" alt="Logo" width={92} height={27} />
           </a>
         </div>
-        <div>
-          <div>
+        <div className="contentForm">
+          <div className="text">
             <h1>Log in</h1>
             <h3>Continue to Shopify account</h3>
           </div>
-          <form>
+          <form className="form">
             <label htmlFor="email">Email</label>
             <input type="email" />
-            <button type="submit">Continue with email</button>
-            <button type="submit">Sign in with passkey</button>
+            <button type="submit" className="emailButton">
+              Continue with email
+            </button>
+            <button type="submit" className="passkey">
+              <User />
+              Sign in with passkey
+            </button>
           </form>
           <p className="sideways">
             <span>or</span>
           </p>
+          <div className="icons">
+            <div className="iconContainer">
+              <AppleIcon />
+            </div>
+            <div className="iconContainer">
+              <FacebookIcon />
+            </div>
+            <div className="iconContainer">
+              <GoogleIcon />
+            </div>
+          </div>
+          <div className="newTo">
+            <h3>New to Shopify</h3>
+            <a href="/">Get started</a>
+            <RightArrow />
+          </div>
         </div>
-        <div>
-          <AppleIcon />
+
+        <div className="bottomLinks">
+          <a href="/">Help</a>
+          <a href="/">Privacy</a>
+          <a href="/">Terms</a>
         </div>
       </div>
     </div>
---
+++ src/svg/arrow.jsx
@@ -0,0 +1,20 @@
+export default function RightArrow() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 46 46"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* <circle cx="23" cy="23" r="23" fill="#F5F5F5" /> */}
+      <path
+        d="M14.5 23H31M31 23L24 16M31 23L24 30"
+        stroke="#005bd3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
