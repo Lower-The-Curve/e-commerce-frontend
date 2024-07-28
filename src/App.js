79b9ba7b0f58f5d6933626fe324b9595853aa8b8
Albin Izmaku <albinizmaku2@gmail.com>
2024-07-29 00:28:01 +0200

    New update

--- src/App.js
+++ src/App.js
@@ -1,63 +1,94 @@
 import "./App.css";
 import AppleIcon from "./svg/apple";
-import RightArrow from "./svg/arrow";
 import FacebookIcon from "./svg/facebook";
 import GoogleIcon from "./svg/google";
+import RightArrow from "./svg/arrow";
 import User from "./svg/user";
 
 function App() {
   return (
-    <div className="main">
-      <div className="card">
+    <main className="main">
+      <div className="square">
         <div>
-          <a href="https://www.shopify.com/" target="_blank" rel="noreferrer">
-            <img src="./logo.svg" alt="Logo" width={92} height={27} />
-          </a>
-        </div>
-        <div className="contentForm">
-          <div className="text">
-            <h1>Log in</h1>
-            <h3>Continue to Shopify account</h3>
-          </div>
-          <form className="form">
-            <label htmlFor="email">Email</label>
-            <input type="email" />
-            <button type="submit" className="emailButton">
-              Continue with email
-            </button>
+          <div className="login-card">
+            <header>
+              <h1 className="login-card_logo">
+                <a href="https://www.shopify.com/" target="_blank" rel="noreferrer">
+                  <img
+                    src="./logo.svg"
+                    alt="Log in to Shopify"
+                    width={100}
+                    height={29}
+                  />
+                </a>
+              </h1>
+            </header>
+
+            <div className="login-card_content">
+              <div>
+                <h1>Log in</h1>
+                <h3>Continue to Shopify account</h3>
+              </div>
+            </div>
+            <form className="form">
+              <div className="emailBox">
+                <label>Email</label>
+                <input type="email" autoFocus size={30} />
+              </div>
+              <button type="submit">
+                Continue with email{" "}
+                <span className="rightArrow">
+                  <RightArrow />
+                </span>
+              </button>
+            </form>
             <button type="submit" className="passkey">
-              <User />
+              <span>
+                <User />
+              </span>
               Sign in with passkey
             </button>
-          </form>
-          <p className="sideways">
+          </div>
+          <div className="divider">
             <span>or</span>
-          </p>
-          <div className="icons">
-            <div className="iconContainer">
-              <AppleIcon />
+          </div>
+          <div className="socialMediaContainer">
+            <div>
+              <a href="/" className="socialMedia">
+                <AppleIcon />
+              </a>
             </div>
-            <div className="iconContainer">
-              <FacebookIcon />
+            <div>
+              <a href="/" className="socialMedia">
+                <FacebookIcon />
+              </a>
             </div>
-            <div className="iconContainer">
-              <GoogleIcon />
+            <div>
+              <a href="/" className="socialMedia">
+                <GoogleIcon />
+              </a>
             </div>
           </div>
-          <div className="newTo">
-            <h3>New to Shopify</h3>
-            <a href="/">Get started</a>
-            <RightArrow />
+          <div className="getStarted">
+            <span>New to Shopify?</span>
+            <a href="/" className="start">
+              Get started
+              <span style={{ width: 20, height: 20 }}>
+                <RightArrow />
+              </span>
+            </a>
           </div>
         </div>
 
-        <div className="bottomLinks">
-          <a href="/">Help</a>
-          <a href="/">Privacy</a>
-          <a href="/">Terms</a>
-        </div>
+        <footer>
+          <div>
+            <a href="/">Help</a>
+            <a href="/">Privacy</a>
+            <a href="/">Terms</a>
+          </div>
+        </footer>
       </div>
-    </div>
+    </main>
   );
 }
 
